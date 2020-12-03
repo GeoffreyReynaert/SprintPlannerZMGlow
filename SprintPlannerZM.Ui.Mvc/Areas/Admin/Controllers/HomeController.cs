485cd0bd3d0f4f575f46f786a8a357a5b7df1b33
Reynaert Geoffrey <Geoffrey.reynaert@student.vives.be>
2020-12-03 10:25:56 +0100

    Admin area gemaakt

---
+++ SprintPlannerZM.Ui.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SprintPlannerZM.Ui.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController: Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SprintPlannerZM.Ui.Mvc/Startup.cs
+++ SprintPlannerZM.Ui.Mvc/Startup.cs
@@ -49,6 +49,10 @@ namespace SprintPlannerZM.Ui.Mvc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
